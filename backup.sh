1fc6bd257f37e777021ee89e926502fbb50b9ba4
julian-bieringer <bieringer.julian@gmail.com>
2018-12-13 17:41:55 +0100

    issue #14 - added parent folder with todays date. added bash_history, pictures and music to backup.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,18 @@
 cd ~/
 echo "searching for ~/backup"
 mkdir -p backup
+today=$(date --iso-8601)
+echo "creating directory for $today"
+mkdir -p backup/$today
+printf "\ncompressing bash_history\n"
+lrztar -z backup/$today/bash_history.tar.lrz -z .bash_history
+echo "done compressing"
 printf "\ncompressing ~/Documents\n"
-lrztar -o backup/Documents.tar.lrz -z Documents
+lrztar -z backup/$today/Documents.tar.lrz -z Documents
+echo "done compressing"
+printf "\ncompressing ~/Pictures\n"
+lrztar -z backup/$today/Pictures.tar.lrz -z Pictures
+echo "done compressing"
+printf "\ncompressing ~/Music\n"
+lrztar -z backup/$today/Music.tar.lrz -z Music
 echo "done compressing"
