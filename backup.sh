bbfc6719736590699a4b677ee8152a6abb3481fb
julian-bieringer <bieringer.julian@gmail.com>
2018-12-29 18:27:20 +0100

    issue #14 - now hidden directories are included in backup as well

--- backup.sh
+++ backup.sh
@@ -4,15 +4,22 @@ mkdir -p backup
 today=$(date --iso-8601)
 echo "creating directory for $today"
 mkdir -p backup/$today
-printf "\ncompressing bash_history\n"
-lrztar -z backup/$today/bash_history.tar.lrz -z .bash_history
+printf "\ncompressing hidden directories\n"
+for f in .[^.]*; do
+	if [ $f == ".config" ] || [ $f == ".cache" ] || [ $f == ".local" ]; then
+		printf "\nomitting $f directory\n";
+		continue;
+	fi
+	printf "\ncompressing $f\n";
+	lrztar -o backup/$today/$f.tar.lrz -z $f;
+done
 echo "done compressing"
 printf "\ncompressing ~/Documents\n"
-lrztar -z backup/$today/Documents.tar.lrz -z Documents
+lrztar -o backup/$today/Documents.tar.lrz -z Documents
 echo "done compressing"
 printf "\ncompressing ~/Pictures\n"
-lrztar -z backup/$today/Pictures.tar.lrz -z Pictures
+lrztar -o backup/$today/Pictures.tar.lrz -z Pictures
 echo "done compressing"
 printf "\ncompressing ~/Music\n"
-lrztar -z backup/$today/Music.tar.lrz -z Music
+lrztar -o backup/$today/Music.tar.lrz -z Music
 echo "done compressing"
