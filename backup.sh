d43d9e83f4fb88b7bfce2b854d88d41bd815b126
julian-bieringer <julian.bieringer@logsta.com>
2020-05-08 13:26:12 +0200

    restructured backup script to be more easy to use

--- backup.sh
+++ backup.sh
@@ -1,25 +1,7 @@
-cd ~/
-echo "searching for ~/backup"
-mkdir -p backup
-today=$(date --iso-8601)
-echo "creating directory for $today"
-mkdir -p backup/$today
-printf "\ncompressing hidden directories\n"
-for f in .[^.]*; do
-	if [ $f == ".config" ] || [ $f == ".cache" ] || [ $f == ".local" ]; then
-		printf "\nomitting $f directory\n";
-		continue;
-	fi
-	printf "\ncompressing $f\n";
-	lrztar -o backup/$today/$f.tar.lrz -z $f;
-done
-echo "done compressing"
-printf "\ncompressing ~/Documents\n"
-lrztar -o backup/$today/Documents.tar.lrz -z Documents
-echo "done compressing"
-printf "\ncompressing ~/Pictures\n"
-lrztar -o backup/$today/Pictures.tar.lrz -z Pictures
-echo "done compressing"
-printf "\ncompressing ~/Music\n"
-lrztar -o backup/$today/Music.tar.lrz -z Music
+cd /tmp
+echo "creating backup"
+dtoday=$(date --iso-8601)
+tar --exclude ~/NO_BACKUP -cvzpf /tmp/$dtoday.tar.gz ~/
+echo "moving to home directory"
+mv $dtoday.tar.gz ~/
 echo "done compressing"
