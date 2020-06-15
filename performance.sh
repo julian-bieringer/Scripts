576e43613a30cb28979ae08d4eb81634b6625f0c
julian-bieringer <julian.bieringer@logsta.com>
2020-06-15 07:00:52 +0200

    performance.sh now sets all cores to performance governor

--- performance.sh
+++ performance.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
-gov=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor);
-echo "current governor is: $gov";
-sudo cpufreq-set -g performance
-gov=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor);
-echo "governor set to: $gov";
+#!/bin/bash
+
+## Enable extended globbing features
+shopt -s extglob
+
+for cpu in /sys/devices/system/cpu/cpu+([0-9]); do
+        core=$(echo $cpu | sed 's:.*/::' | tr -d -c 0-9);
+	gov=$(cat $cpu/cpufreq/scaling_governor);
+	echo "current governor for $core is: $gov";
+	sudo cpufreq-set --cpu $core -g performance
+	gov=$(cat $cpu/cpufreq/scaling_governor);
+        echo "governor for $core set to: $gov";
+done
+
+echo ""
+echo "verification run"
+echo ""
+
+for cpu in /sys/devices/system/cpu/cpu+([0-9]); do
+        core=$(echo $cpu | sed 's:.*/::' | tr -d -c 0-9);
+        gov=$(cat $cpu/cpufreq/scaling_governor);
+        echo "current governor for $core is: $gov";
+done
