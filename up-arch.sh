c5c93a423a96cb2a4c9bfdadb30567831f9e9f59
julian-bieringer <bieringer.julian@gmail.com>
2022-05-29 12:37:41 +0200

    including development packages into up-arch.sh

--- up-arch.sh
+++ up-arch.sh
@@ -5,11 +5,11 @@ echo -e "\033[32m**************************************************\033[0m"
 sudo pacman -Syyu
 echo ""
 echo -e "\033[32m**************************************************\033[0m"
-echo -e "\033[32m*              pamac checkupdates -a             *\033[0m"
+echo -e "\033[32m*         pamac checkupdates -a --devel          *\033[0m"
 echo -e "\033[32m**************************************************\033[0m"
-pamac checkupdates -a
+pamac checkupdates -a --devel
 echo ""
 echo -e "\033[32m**************************************************\033[0m"
-echo -e "\033[32m*                pamac upgrade -a                *\033[0m"
+echo -e "\033[32m*            pamac upgrade -a --devel            *\033[0m"
 echo -e "\033[32m**************************************************\033[0m"
-pamac upgrade -a
+pamac upgrade -a --devel
