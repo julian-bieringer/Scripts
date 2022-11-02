0f25a89d3148962a292e1237af087b142d7da4b0
julian-bieringer <bieringer.julian@gmail.com>
2022-11-02 14:23:21 +0100

    issue #19 - now allowing optional flags like --audioFormat

--- youtube-dl-batch.sh
+++ youtube-dl-batch.sh
@@ -3,22 +3,63 @@
 # citation for coshell
 # https://github.com/gdm85/coshell
 
-if [ $# -ne 2 ]; then
-	echo "Too few arguments provided! Correct usage: youtube-dl-batch <input_file> <output_directory>"; 
-	exit 1;
-fi;
+# define default values for optional flags
+
+audioFormat="mp3"
+
+# define mandatory flags
+
+urlFile=""
+outputDirectory=""
+
+# process parameters
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --inputFile=*)
+      urlFile="${1#*=}"
+      urlFile="${urlFile//\~/$HOME}"
+      echo "Set input file to: $urlFile"
+      ;;
+    --outputDirectory=*)
+      outputDirectory="${1#*=}"
+      outputDirectory="${outputDirectory//\~/$HOME}"
+      echo "Set output directory to: $outputDirectory"
+      ;;
+    --audioFormat=*)
+      audioFormat="${1#*=}"
+      echo "Overrode audio format to: $audioFormat"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
 
 
-urlFile=$1
-outputDirectory=$2;
-coShellInput=""
+# check mandatory parameters are set
+
+if [[ -z "$urlFile" ]]; then
+    echo "--inputFile needs to be set to provide the script with the path to the file with the youtube urls"
+    exit 1
+fi
+
+if [[ -z "$outputDirectory" ]]; then
+    echo "--outputDirectory needs to be set to provide the script with the path to directory where the files should be outputed"
+    exit 1
+fi
+
+coShellInput="";
 
 if [ ${outputDirectory:${#outputDirectory}-1:1} == '/' ]; then #check if output directory ends on "/", e.g. /home/{USER}/Music/
     outputDirectory=${outputDirectory:0:${#outputDirectory}-1} #if output directory ends on "/", remove the last character so the output will look like /home/{USER}/Music
 fi;
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    coShellInput=$coShellInput$"youtube-dl -o \"$outputDirectory/%(title)s.%(ext)s\" --extract-audio --audio-format mp3 --audio-quality 0 --external-downloader aria2c --external-downloader-args \"-x 16 -s 16 -k 1M\" $line\n";
+    coShellInput=$coShellInput$"youtube-dl -o \"$outputDirectory/%(title)s.%(ext)s\" --extract-audio --audio-format $audioFormat --audio-quality 0 --external-downloader aria2c --external-downloader-args \"-x 16 -s 16 -k 1M\" $line\n";
 done < $urlFile #for each line (url) in the file call youtube-dl with outputDirectory\title.extension and use aria2c as a downloader to support parallel download connections
 
 echo -ne $coShellInput
